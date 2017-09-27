2198fc4e90e97f6c3503e726c37d2d1b24ed1266
Gaurav Tiwari <gauravtiwari5050@gmail.com>
2017-09-27 23:49:42 +0530

    basic form rendering

--- test/formic.js
+++ test/formic.js
@@ -3,7 +3,12 @@ const Formic = require('../index.js');
 
 test('basic form test', function (t) { // eslint-disable-line
   let f = new Formic.FormFor("user");
-  f.klass('hello')
+  f.klass('classval').style({'width':'800px','height':'100px'}).id("index");
+  f.url('/');
+
+  f.emailField('user_email');
+  f.emailField('xyz');
+
   console.log(f.toString());
   t.end();
 });
